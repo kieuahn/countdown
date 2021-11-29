6d6eb32e4b3a428816af1643d6b8c14f2735a29e
kieuahn <73641824+kieuahn@users.noreply.github.com>
2021-11-29 18:57:49 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 input.onButtonPressed(Button.A, function () {
-    if (set_Timerseconds < 50) {
-        set_Timerseconds += 10
-        basic.showNumber(set_Timerseconds)
+    if (set_Timerminutes < 59) {
+        set_Timerminutes += 1
+        basic.showNumber(set_Timerminutes)
+        basic.showString(":" + set_Timerseconds)
+        basic.pause(100)
+        basic.clearScreen()
+    } else {
+        set_Timerminutes = 0
+        set_Timerhours += 1
+        basic.showNumber(set_Timerhours)
+        basic.showString("" + set_Timerhours + ":" + set_Timerminutes + ":" + set_Timerseconds)
         basic.pause(100)
         basic.clearScreen()
     }
@@ -22,20 +30,10 @@ input.onButtonPressed(Button.B, function () {
         basic.clearScreen()
     } else {
         set_Timerseconds = 0
-        basic.showString("" + set_Timerminutes + ":" + set_Timerseconds)
         set_Timerminutes += 1
+        basic.showString("" + set_Timerminutes + ":" + "0" + set_Timerseconds)
         basic.pause(100)
         basic.clearScreen()
-        if (set_Timerminutes < 59) {
-            set_Timerminutes = 0
-            basic.showNumber(set_Timerhours)
-            basic.showString("" + set_Timerhours + ":" + set_Timerminutes + ":" + set_Timerseconds)
-            set_Timerhours += 1
-            basic.pause(100)
-            basic.clearScreen()
-        } else {
-            set_Timerhours = 0
-        }
     }
 })
 let set_Timerhours = 0
